62d320d465dc8036ddcb27b9a63920031f7fc3dd
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-02-19 14:06:42 -0500

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [item for item in num_list if (item%2 ==0)]
+    # pass
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence +"!" for sentence in sentence_list]
+    # pass
